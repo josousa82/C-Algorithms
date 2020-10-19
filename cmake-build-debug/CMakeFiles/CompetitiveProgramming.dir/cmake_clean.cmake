94761f5a925530b3f20449f576c917de682309b9
sousajo <josesousa@rentalcars.com>
2020-10-19 23:00:28 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompetitiveProgramming.dir/linked_list/linkedlist.cpp.o"
+  "CMakeFiles/CompetitiveProgramming.dir/main.cpp.o"
+  "CompetitiveProgramming"
+  "CompetitiveProgramming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompetitiveProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//int main() {
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//
+//    int a = 21, b;
+//    int c, d;
+//
+//    string s;
+//    getline(cin, s);
+//    printf("%s\n", s.c_str());
+//
+//    string str = "Hello, World!" ;
+//    string x;
+//    cout << "Enter a string and 2 ints" << endl;
+//    cin >> x >> a >> b;
+//    for (int i = 0; i < 5; ++i) {
+//        cout << x << " " << a << " " << b << "\n"<< endl;
+//    }
+//
+//    cout << "Enter 2 ints (scanf example)" << endl;
+//    scanf("%d ", &c);
+//    scanf("%d ", &d);
+//    printf("%d,  %d\n", c, d);
+//
+//
+//
+//    return 0;
+//}
